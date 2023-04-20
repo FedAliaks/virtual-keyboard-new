efd8b51dff67dcb0bbed57fb0dc3c34e7f6fa9fa
Aliaksandr Fedukovich <1632637@mail.ru>
2023-04-20 20:44:27 +0300

    init: add start files and complect letters for EN and RU

---
+++ virtual-keyboard/src/index.js
@@ -0,0 +1,9 @@
+
+import './style.scss';
+const img = require('./assets/imageSea.jpg');
+
+
+document.body.innerHTML = `<h1>i want to go home</h1>
+<img src="${img}" alt="tratata"`;
+
+alert('12341234');
---
+++ virtual-keyboard/src/js/keyboards.js
@@ -0,0 +1,19 @@
+const KEYBOARD_FIRST_EN = ['`', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', 'Backspace'];
+const KEYBOARD_FIRST_EN_SHIFT = ['~', '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '_', '=', 'Backspace'];
+const KEYBOARD_SECOND_EN = ['Tab', 'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', '[', ']', '\\', 'Del'];
+const KEYBOARD_SECOND_EN_SHIFT = ['Tab', 'Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P', '{', '}', '|', 'Del'];
+const KEYBOARD_THIRD_EN = ['Caps Lock', 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';', '\'', 'Enter'];
+const KEYBOARD_THIRD_EN_SHIFT = ['Caps Lock', 'A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L', ':', '"', 'Enter'];
+const KEYBOARD_FOURTH_EN = ['Shift', 'z', 'x', 'c', 'v', 'b', 'n', 'm', ',', '.',  '/', 'Arrow Up', 'Shift'];
+const KEYBOARD_FOURTH_EN_SHIFT = ['Shift', 'Z', 'X', 'C', 'V', 'B', 'N', 'M', '<', '>', '?', 'Arrow Up', 'Shift'];
+const KEYBOARD_FIFTH_EN = ['Ctrl', 'Win', 'Space', 'Alt', 'Ctrl', "Arrow Left", 'Arrow Down', 'Arrow Right'];
+
+const KEYBOARD_FIRST_RU = ['`', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', 'Backspace'];
+const KEYBOARD_FIRST_RU_SHIFT = ['~', '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '_', '=', 'Backspace'];
+const KEYBOARD_SECOND_RU = ['Tab', 'й', 'ц', 'у', 'к', 'е', 'н', 'г', 'ш', 'щ', 'з', 'х', 'ъ', '\\', 'Del'];
+const KEYBOARD_SECOND_RU_SHIFT = ['Tab', 'Й', 'Ц', 'У', 'К', 'Е', 'Н', 'Г', 'Ш', 'Щ', 'З', 'З', 'Х', 'Ъ', 'Del'];
+const KEYBOARD_THIRD_RU = ['Caps Lock', 'ф', 'ы', 'в', 'а', 'п', 'р', 'о', 'л', 'д', 'ж', 'э', 'Enter'];
+const KEYBOARD_THIRD_RU_SHIFT = ['Caps Lock', 'Ф', 'Ы', 'В', 'А', 'П', 'Р', 'О', 'Л', 'Д', 'Ж', 'Э', 'Enter'];
+const KEYBOARD_FOURTH_RU = ['Shift', 'я', 'ч', 'с', 'м', 'и', 'т', 'ь', 'б', 'ю',  '.', 'Arrow Up', 'Shift'];
+const KEYBOARD_FOURTH_RU_SHIFT = ['Shift', 'Я', 'Ч', 'С', 'М', 'И', 'Т', 'Ь', 'Б', 'Ю', ',', 'Arrow Up', 'Shift'];
+const KEYBOARD_FIFTH_RU = ['Ctrl', 'Win', 'Space', 'Alt', 'Ctrl', "Arrow Left", 'Arrow Down', 'Arrow Right'];
