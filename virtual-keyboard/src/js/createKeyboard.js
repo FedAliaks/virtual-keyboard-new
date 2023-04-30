e9093b3471e38cb41e523178dc77be65c76f53f0
Aliaksandr Fedukovich <1632637@mail.ru>
2023-04-30 18:04:06 +0300

    feat: createeyboard.js lint. part1

--- virtual-keyboard/src/js/createKeyboard.js
+++ virtual-keyboard/src/js/createKeyboard.js
@@ -1,23 +1,20 @@
 import { Button } from './Button';
 
-
 function createKeyboardLines(language, btnText) {
   const KEYBOARD_SECTION = document.querySelector('.keyboard');
   KEYBOARD_SECTION.innerHTML = '';
 
-  language.forEach(keyboardLine => {
-    let line = document.createElement('div');
+  language.forEach((keyboardLine) => {
+    const line = document.createElement('div');
     line.className = 'keyboard-line';
-    keyboardLine.forEach(item => {
-      let btn = new Button(item);
-      let btnElement = btn.createButton(btnText);
+    keyboardLine.forEach((item) => {
+      const btn = new Button(item);
+      const btnElement = btn.createButton(btnText);
       line.append(btnElement);
     });
 
     KEYBOARD_SECTION.appendChild(line);
-
-  })
-
+  });
 }
 
-export { createKeyboardLines }
+export { createKeyboardLines };
