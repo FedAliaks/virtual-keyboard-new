4570d7b584b0af9c2132e784ec0bad46cde4c80d
Aliaksandr Fedukovich <1632637@mail.ru>
2023-04-30 18:08:44 +0300

    feat: add lint for button.js

--- virtual-keyboard/src/js/Button.js
+++ virtual-keyboard/src/js/Button.js
@@ -1,6 +1,4 @@
-import { sizeLetter } from "../index";
-
-
+import { sizeLetter } from '../index';
 
 export class Button {
   constructor(symbol) {
@@ -8,27 +6,26 @@ export class Button {
   }
 
   createButton(btnText) {
-    let div = document.createElement('div');
+    const div = document.createElement('div');
     div.className = 'button';
-    if(this.innerTextButton.length > 1) {
+    if (this.innerTextButton.length > 1) {
       div.classList.add('button-dark');
     }
 
-    if(this.innerTextButton === 'Space') {
+    if (this.innerTextButton === 'Space') {
       div.classList.remove('button-dark');
       div.classList.add('button-space');
     }
 
-    if(this.innerTextButton === 'CapsLock' && sizeLetter === 'big' && btnText === 'CapsLock') {
+    if (this.innerTextButton === 'CapsLock' && sizeLetter === 'big' && btnText === 'CapsLock') {
       div.classList.add('button_active');
     }
 
-    if(this.innerTextButton === 'Shift' && sizeLetter === 'big' &&  btnText === 'Shift') {
-/*       console.log(123) */
+    if (this.innerTextButton === 'Shift' && sizeLetter === 'big' && btnText === 'Shift') {
       div.classList.add('button_active');
     }
 
-    const text = this.innerTextButton /* === "Space" ? '' : `${this.innerTextButton}`; */
+    const text = this.innerTextButton;
 
     div.innerText = text;
 
